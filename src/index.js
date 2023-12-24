1d0234e4ff9b01438803641aa9fe9e0ade59fd3c
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-12-25 00:55:16 +0300

    added functionality to other pages

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { smoothScroll } from "./modules/scroll";
 import { serviceSlider } from "./modules/serviceSlider";
 import { callASpecialist } from "./modules/callASpecialist";
 import { timer } from "./modules/timer";
+import { validForm } from "./modules/validForm";
 
 requestACall();
 sliderCarousel();
@@ -11,3 +12,4 @@ smoothScroll();
 serviceSlider();
 callASpecialist();
 timer("31 december 2023");
+validForm();
