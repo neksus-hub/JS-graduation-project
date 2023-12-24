a9d86ea3027289d9fc6bb320282bbde0619b8e8a
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-12-25 00:55:30 +0300

    added functionality to other pages

---
+++ src/modules/validForm.js
@@ -0,0 +1,7 @@
+export const validForm = () => {
+  const nameInput = document.querySelector(".form-control[name=fio]");
+  const phoneInput = document.querySelector(".form-control[name=phone]");
+  const form = document.querySelector(".form-horizontal");
+
+  console.log(form);
+};
