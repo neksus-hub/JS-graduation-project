26dd665c1876fcee694bc43491a462373681d55f
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-12-27 01:49:11 +0300

    added hidden scroll

--- src/modules/callASpecialist.js
+++ src/modules/callASpecialist.js
@@ -1,6 +1,7 @@
 export const callASpecialist = () => {
   const modal = document.querySelector(".services-modal");
   const overlay = document.querySelector(".overlay");
+  const body = document.querySelector("body");
 
   const showModal = () => {
     modal.style.display = "block";
@@ -12,11 +13,30 @@ export const callASpecialist = () => {
     overlay.style.display = "none";
   };
 
+  const hideScroll = () => {
+    let div = document.createElement("div");
+
+    div.style.overflowY = "scroll";
+    div.style.width = "50px";
+    div.style.height = "50px";
+
+    document.body.append(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+
+    div.remove();
+
+    body.style.paddingRight = `${scrollWidth}px`;
+    body.style.overflow = "hidden";
+  };
+
   document.addEventListener("click", (e) => {
     if (e.target.closest(".service-button")) {
       showModal();
+      hideScroll();
     } else if (e.target.closest(".services-modal__close")) {
       hideModal();
+      body.style.paddingRight = `0`;
+      body.style.overflow = "visible";
     }
   });
 };
--- src/modules/requestACall.js
+++ src/modules/requestACall.js
@@ -1,10 +1,27 @@
 export const requestACall = () => {
   const modal = document.querySelector(".header-modal");
   const overlay = document.querySelector(".overlay");
+  const body = document.querySelector("body");
 
   const showModal = () => {
     modal.style.display = "block";
     overlay.style.display = "block";
+  };
+
+  const hideScroll = () => {
+    let div = document.createElement("div");
+
+    div.style.overflowY = "scroll";
+    div.style.width = "50px";
+    div.style.height = "50px";
+
+    document.body.append(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+
+    div.remove();
+
+    body.style.paddingRight = `${scrollWidth}px`;
+    body.style.overflow = "hidden";
   };
 
   const hideModal = () => {
@@ -15,8 +32,11 @@ export const requestACall = () => {
   document.addEventListener("click", (e) => {
     if (e.target.closest("div.col-sm-12>.button")) {
       showModal();
+      hideScroll();
     } else if (e.target.closest(".header-modal__close")) {
       hideModal();
+      body.style.paddingRight = `0`;
+      body.style.overflow = "visible";
     }
   });
 };
